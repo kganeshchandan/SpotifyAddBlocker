3a9135598446a539a28126b8b062d6e0e12d7a5d
shinigami001 <chanduuzumaki01@gmail.com>
2021-01-30 20:34:48 +0530

    music resumes on its own now

--- spotify.sh
+++ spotify.sh
@@ -7,7 +7,7 @@ function spotstart {
   grep -m 1 spotify <( exec dbus-monitor "member='AddMatch'" ) >/dev/null
   kill $! 2>/dev/null
   sleep 1
-  echo "[Launched Spotify!]"
+  echo "[      Launched Spotify!      ]"
 }
 
 function spotplay {
@@ -20,12 +20,13 @@ temp2=$(whoami)
 while true ;do
  temp1=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -Ev "^method"  | grep -Eo '("(.*)")|(\b[0-9][a-zA-Z0-9.]*\b)' | sed -E '2~2 a|' | tr -d '\n'|sed -E 's/\|/\n/g'| sed -E 's/(xesam:)|(mpris:)//' | sed -E 's/^"//'| sed -E 's/"$//'| sed -E 's/"+/|/' | grep "title")
   if [ "$temp1" = "title|Advertisement" ]; then
-    echo "inside if"
+    echo "[      Add detected      ]"
     killall spotify
     grep -m 1 spotify <( exec dbus-monitor "member='RemoveMatch'" ) >/dev/null
   kill $! 2>/dev/null
-  sleep 2
+ 
     spotstart
+     sleep 5
     spotplay
   else
     if [ "$temp1" != "$temp2" ]; then 
@@ -36,4 +37,5 @@ while true ;do
   done
 }
 echo "[Listening...]"
+echo "[The tracks you are Listening ....]"
 main
